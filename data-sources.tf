9b839a5b914c1e58edac163b274fa27106a964a4
51r <petar.spasov@hashicorp.com>
2022-05-30 15:16:21 +0300

    modified data-sources and main.tf

--- data-sources.tf
+++ data-sources.tf
@@ -1,6 +1,18 @@
 data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = ["099720109477"]
+  provider    = aws.i1
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-*-amd64-server-*"]
+  }
+}
+
+data "aws_ami" "ubuntu2" {
+  most_recent = true
+  owners      = ["099720109477"]
+  provider    = aws.i2
 
   filter {
     name   = "name"
