5146d51c5d683be351dda961ab4c294848f53750
51r <petar.spasov@hashicorp.com>
2022-05-30 14:16:48 +0300

    added example + modified the config

--- data-sources.tf
+++ data-sources.tf
@@ -1,15 +1,4 @@
 data "aws_ami" "ubuntu" {
-  provider = aws.ew2
-  most_recent = true
-  owners      = ["099720109477"]
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-*-amd64-server-*"]
-  }
-}
-data "aws_ami" "ubuntu2" {
-    provider = aws.ec1
   most_recent = true
   owners      = ["099720109477"]
 
--- main.tf
+++ main.tf
@@ -6,31 +6,10 @@ terraform {
   }
 }
 
-provider "aws" {
-  alias  = "ew2"
-  region = "eu-west-2"
-}
-
-provider "aws" {
-  alias  = "ec1"
-  region = "eu-central-1"
-}
-
 resource "aws_instance" "ubuntu" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  provider      = aws.ew2
-  tags = {
-    Name  = "Peter-Test-Ubuntu"
-    Owner = "petar.spasov@hashicorp.com"
-  }
-}
-
-resource "aws_instance" "ubuntu2" {
-  ami           = data.aws_ami.ubuntu2.id
-  instance_type = "t2.micro"
-  provider      = aws.ec1
-  tags = {
+    tags = {
     Name  = "Peter-Test-Ubuntu"
     Owner = "petar.spasov@hashicorp.com"
   }
