fee16184f806c71918ff000164d6e60f27dd86e2
51r <petar.spasov@hashicorp.com>
2022-05-30 15:06:48 +0300

    modified the code

--- main.tf
+++ main.tf
@@ -1,15 +1,29 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source                = "hashicorp/aws"
+      configuration_aliases = [ aws.i1, aws.i2 ]
     }
   }
 }
 
-resource "aws_instance" "ubuntu" {
+resource "aws_instance" "instance1" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-    tags = {
+  provider = aws.i1
+
+  tags = {
+    Name  = "Peter-Test-Ubuntu"
+    Owner = "petar.spasov@hashicorp.com"
+  }
+}
+
+resource "aws_instance" "instance2" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  provider = aws.i2
+  
+  tags = {
     Name  = "Peter-Test-Ubuntu"
     Owner = "petar.spasov@hashicorp.com"
   }
